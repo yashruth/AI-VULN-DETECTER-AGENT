9be5104c7c49c839b56e9f79d69d7cf13dba7bf6
Ashruth yenishetty <116997774+yashruth@users.noreply.github.com>
2026-04-23 10:57:32 +0530

    Update report.py

--- report.py
+++ report.py
@@ -7,9 +7,9 @@ def generate_report(vulns):
     pdf.add_page()
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(200,10,"AI Vulnerability Report", ln=True)
+    pdf.cell(200,10,"AI Vulnerability Assessment Report", ln=True)
     pdf.cell(200,10,f"Total Findings: {len(vulns)}", ln=True)
-    pdf.cell(200,10,"----------------------------", ln=True)
+    pdf.cell(200,10,"--------------------------------------", ln=True)
 
     if len(vulns) == 0:
         pdf.cell(200,10,"No vulnerabilities found", ln=True)
@@ -18,12 +18,28 @@ def generate_report(vulns):
 
         impact, rec = get_details(v["vulnerability"])
 
-        pdf.cell(200,10,"Vuln: "+v["vulnerability"], ln=True)
-        pdf.cell(200,10,"Severity: "+v["severity"], ln=True)
-        pdf.cell(200,10,"CVSS: "+str(v["cvss"]), ln=True)
-        pdf.cell(200,10,"Impact: "+impact, ln=True)
-        pdf.cell(200,10,"Recommendation: "+rec, ln=True)
-        pdf.cell(200,10,"----------------------", ln=True)
+        pdf.set_font("Arial","B",12)
+        pdf.cell(200,10,"Vulnerability:", ln=True)
+
+        pdf.set_font("Arial", size=12)
+        pdf.multi_cell(0,10,v["vulnerability"])
+
+        pdf.cell(200,10,"Severity: " + v["severity"], ln=True)
+        pdf.cell(200,10,"CVSS: " + str(v["cvss"]), ln=True)
+
+        pdf.set_font("Arial","B",12)
+        pdf.cell(200,10,"Impact:", ln=True)
+
+        pdf.set_font("Arial", size=12)
+        pdf.multi_cell(0,10,impact)
+
+        pdf.set_font("Arial","B",12)
+        pdf.cell(200,10,"Recommendation:", ln=True)
+
+        pdf.set_font("Arial", size=12)
+        pdf.multi_cell(0,10,rec)
+
+        pdf.cell(200,10,"--------------------------------------", ln=True)
 
     path = "report.pdf"
     pdf.output(path)
