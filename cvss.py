56d555956fe847f9376d5d69a49040a627b6bedb
Ashruth yenishetty <116997774+yashruth@users.noreply.github.com>
2026-04-23 10:21:18 +0530

    Update cvss.py

--- cvss.py
+++ cvss.py
@@ -2,12 +2,9 @@ def calculate_cvss(severity):
 
     if severity == "Critical":
         return 9.5
-
     elif severity == "High":
         return 8.0
-
     elif severity == "Medium":
         return 6.0
-
     else:
         return 3.0
