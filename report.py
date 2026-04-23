306045dd929c9d315ed7da8a6a516754c61fb520
Ashruth yenishetty <116997774+yashruth@users.noreply.github.com>
2026-04-23 10:50:52 +0530

    Update report.py

--- report.py
+++ report.py
@@ -7,22 +7,23 @@ def generate_report(vulns):
     pdf.add_page()
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(200,10,"Vulnerability Report", ln=True)
+    pdf.cell(200,10,"AI Vulnerability Report", ln=True)
+    pdf.cell(200,10,f"Total Findings: {len(vulns)}", ln=True)
+    pdf.cell(200,10,"----------------------------", ln=True)
 
-    if len(vulns)==0:
+    if len(vulns) == 0:
         pdf.cell(200,10,"No vulnerabilities found", ln=True)
 
     for v in vulns:
 
         impact, rec = get_details(v["vulnerability"])
 
-        pdf.cell(200,10,v["vulnerability"], ln=True)
-        pdf.cell(200,10,v["severity"], ln=True)
-        pdf.cell(200,10,str(v["cvss"]), ln=True)
-        pdf.cell(200,10,impact, ln=True)
-        pdf.cell(200,10,rec, ln=True)
-
-        pdf.cell(200,10,"----------------", ln=True)
+        pdf.cell(200,10,"Vuln: "+v["vulnerability"], ln=True)
+        pdf.cell(200,10,"Severity: "+v["severity"], ln=True)
+        pdf.cell(200,10,"CVSS: "+str(v["cvss"]), ln=True)
+        pdf.cell(200,10,"Impact: "+impact, ln=True)
+        pdf.cell(200,10,"Recommendation: "+rec, ln=True)
+        pdf.cell(200,10,"----------------------", ln=True)
 
     path = "report.pdf"
     pdf.output(path)
