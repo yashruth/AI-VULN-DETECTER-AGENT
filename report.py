8bf629704588ae9017d54c05881350a67568aff1
Ashruth yenishetty <116997774+yashruth@users.noreply.github.com>
2026-04-23 10:21:46 +0530

    Update report.py

--- report.py
+++ report.py
@@ -5,20 +5,26 @@ def generate_report(vulns):
 
     pdf = FPDF()
     pdf.add_page()
-
     pdf.set_font("Arial", size=12)
 
-    pdf.cell(200,10,"AI Vulnerability Scan Report", ln=True)
+    pdf.cell(200,10,"Vulnerability Report", ln=True)
+
+    if len(vulns)==0:
+        pdf.cell(200,10,"No vulnerabilities found", ln=True)
 
     for v in vulns:
 
-        details = get_details(v["vulnerability"])
+        impact, rec = get_details(v["vulnerability"])
 
-        pdf.cell(200,10,f"Vulnerability: {v['vulnerability']}", ln=True)
-        pdf.cell(200,10,f"Risk: {v['risk']}", ln=True)
-        pdf.cell(200,10,f"Impact: {details['impact']}", ln=True)
-        pdf.cell(200,10,f"Recommendation: {details['recommendation']}", ln=True)
+        pdf.cell(200,10,v["vulnerability"], ln=True)
+        pdf.cell(200,10,v["severity"], ln=True)
+        pdf.cell(200,10,str(v["cvss"]), ln=True)
+        pdf.cell(200,10,impact, ln=True)
+        pdf.cell(200,10,rec, ln=True)
 
-        pdf.cell(200,10,"---------------------------------", ln=True)
+        pdf.cell(200,10,"----------------", ln=True)
 
-    pdf.output("security_report.pdf")
+    path = "report.pdf"
+    pdf.output(path)
+
+    return path
